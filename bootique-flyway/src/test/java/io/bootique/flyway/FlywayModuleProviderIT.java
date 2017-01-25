17119aeb0bf09831fdcfd9f37bf41af58dd2ff6e
Andrus Adamchik <andrus@objectstyle.com>
2017-01-25 18:18:25 +0300

    using BQ test module

--- bootique-flyway/src/test/java/io/bootique/flyway/FlywayModuleProviderIT.java
+++ bootique-flyway/src/test/java/io/bootique/flyway/FlywayModuleProviderIT.java
@@ -1,22 +1,12 @@
 package io.bootique.flyway;
 
-import io.bootique.BQModuleProvider;
+import io.bootique.test.junit.BQModuleProviderChecker;
 import org.junit.Test;
-
-import java.util.ServiceLoader;
-import java.util.stream.StreamSupport;
-
-import static org.junit.Assert.assertEquals;
 
 public class FlywayModuleProviderIT {
 
     @Test
     public void testPresentInJar() {
-
-        long c = StreamSupport
-                .stream(ServiceLoader.load(BQModuleProvider.class).spliterator(), false)
-                .filter(p -> p instanceof FlywayModuleProvider)
-                .count();
-        assertEquals("No provider found", 1, c);
+        BQModuleProviderChecker.testPresentInJar(FlywayModuleProvider.class);
     }
 }
