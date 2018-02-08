40fe296c3c4e3a10a9cfac6dc01fe7ebd2b3c7e5
Ruslan Ibragimov <ruslan@ibragimov.by>
2018-02-08 20:51:49 +0300

    Add JdbcModule to dependencies in FlywayModule. bootique/bootique#203

--- bootique-flyway/src/test/java/io/bootique/flyway/FlywayModuleProviderIT.java
+++ bootique-flyway/src/test/java/io/bootique/flyway/FlywayModuleProviderIT.java
@@ -1,12 +1,27 @@
 package io.bootique.flyway;
 
+import io.bootique.BQRuntime;
+import io.bootique.jdbc.JdbcModule;
 import io.bootique.test.junit.BQModuleProviderChecker;
+import io.bootique.test.junit.BQTestFactory;
+import org.junit.Rule;
 import org.junit.Test;
 
+import static com.google.common.collect.ImmutableList.of;
+
 public class FlywayModuleProviderIT {
+
+    @Rule
+    public BQTestFactory testFactory = new BQTestFactory();
 
     @Test
     public void testPresentInJar() {
         BQModuleProviderChecker.testPresentInJar(FlywayModuleProvider.class);
+    }
+
+    @Test
+    public void testModuleDeclaresDependencies() {
+        final BQRuntime bqRuntime = testFactory.app().module(new FlywayModuleProvider()).createRuntime();
+        BQModuleProviderChecker.testModulesLoaded(bqRuntime, of(JdbcModule.class, FlywayModule.class));
     }
 }
