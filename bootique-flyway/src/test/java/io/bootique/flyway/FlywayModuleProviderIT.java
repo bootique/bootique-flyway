cb4b87b6e8c67c00d3c6aa2ee7fced08dcc3c02f
Andrus Adamchik <andrus@objectstyle.com>
2018-02-18 10:34:10 +0300

    Declare module dependencies bootique/bootique#203
    * new test API

--- bootique-flyway/src/test/java/io/bootique/flyway/FlywayModuleProviderIT.java
+++ bootique-flyway/src/test/java/io/bootique/flyway/FlywayModuleProviderIT.java
@@ -3,11 +3,10 @@ package io.bootique.flyway;
 import io.bootique.BQRuntime;
 import io.bootique.jdbc.JdbcModule;
 import io.bootique.test.junit.BQModuleProviderChecker;
+import io.bootique.test.junit.BQRuntimeChecker;
 import io.bootique.test.junit.BQTestFactory;
 import org.junit.Rule;
 import org.junit.Test;
-
-import static com.google.common.collect.ImmutableList.of;
 
 public class FlywayModuleProviderIT {
 
@@ -22,6 +21,6 @@ public class FlywayModuleProviderIT {
     @Test
     public void testModuleDeclaresDependencies() {
         final BQRuntime bqRuntime = testFactory.app().module(new FlywayModuleProvider()).createRuntime();
-        BQModuleProviderChecker.testModulesLoaded(bqRuntime, of(JdbcModule.class, FlywayModule.class));
+        BQRuntimeChecker.testModulesLoaded(bqRuntime, JdbcModule.class, FlywayModule.class);
     }
 }
