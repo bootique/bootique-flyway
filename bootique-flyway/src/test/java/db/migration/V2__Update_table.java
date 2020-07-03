45623d73afc9b0fbaa470c8d128645cde02828ee
Andrus Adamchik <andrus@objectstyle.com>
2020-07-03 13:07:14 +0300

    removing commented out import

--- bootique-flyway/src/test/java/db/migration/V2__Update_table.java
+++ bootique-flyway/src/test/java/db/migration/V2__Update_table.java
@@ -19,13 +19,11 @@
 
 package db.migration;
 
-//import org.flywaydb.core.api.migration.jdbc.JdbcMigration;
 import org.flywaydb.core.api.migration.BaseJavaMigration;
 import org.flywaydb.core.api.migration.Context;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
